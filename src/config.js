e219a6b3ae896252b612691b8c3266dc3990c500
Mike Blakley <m.blakley@icloud.com>
2019-11-07 09:43:25 -0500

    cors set for zeit origin

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 4050,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://terra-app.mblakley4.now.sh'
 }
