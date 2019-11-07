a24c810012b18b33ea8a0db4d2fcfaa370c5d678
Mike Blakley <m.blakley@icloud.com>
2019-11-07 09:31:34 -0500

    init setup

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+app.get('/api/*', (req, res) => {
+  res.json({ok: true})
 })
 
 app.use(function errorHandler(error, req, res, next) {
