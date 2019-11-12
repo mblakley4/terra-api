9a07bd41d506f16a318c706377b68c0945cbca50
Mike Blakley <m.blakley@icloud.com>
2019-11-12 11:24:00 -0500

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Terra App API
 
-Server-side application supporting front end React App Noteful
+Server-side application supporting The **Terra** App.
+
+See the Client Repo here:  https://github.com/mblakley4/terra-app
+Visit the Live Site: https://terra-app.mblakley4.now.sh
+
+
+# Technology Used:
+  - JavaScript
+  - Node.JS w/ Express
+  - PostgreSQL
+  - Deployed on Heroku
--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://michaelblakley@localhost/terra',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://michaelblakley@localhost/terra-test',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:4050/api"
 }
